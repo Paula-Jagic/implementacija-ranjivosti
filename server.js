4f18194c0dc09475e0d36f196917baa1bbc8ab10
Paula-Jagic <pj55077@fer.hr>
2025-11-09 19:23:59 +0100

    Implementirane XSS i Broken Access Control ranjivosti

--- server.js
+++ server.js
@@ -187,7 +187,8 @@ app.post('/toggle-xss', (req, res) => {
     res.redirect('/');
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server running: http://localhost:3000`);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
 });
